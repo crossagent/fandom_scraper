13853cba8d1a2a9e8ef7b74e0f58b69f96596e65
zhouxy <150273112@qq.com>
2023-07-28 18:24:53 +0800

    支持内容抓取

--- main.py
+++ main.py
@@ -175,6 +175,7 @@ class WikiCategory(WikiAPI):
             self.titles = sorted([x[1] for x in self.pages])
 
     def get_category_members(self, categories=None, recursive=None, group_pages=None, params=None):
+        # 处理参数的默认值
         if categories is None:
             categories = self.categories
         if recursive is None:
@@ -192,32 +193,41 @@ class WikiCategory(WikiAPI):
             items['pages'] = []
 
         print('Retrieving category members:\n')
+        # 对每个类别进行遍历
         for category in tqdm(items['categories']):
+            # 设置API请求的参数
             params['cmtitle'] = f'Category:{category}'
             params['cmcontinue'] = 0
+            # 循环直到所有成员都被获取完
             while params['cmcontinue'] != 1:
                 r = requests.get(API_URL, params=params)
                 # print(r.url)
                 data = json.loads(r.text)
                 results = data['query']['categorymembers']
+                # 获取子类别的标题
                 subcats = [x['title'].replace('Category:', '') for x in results if int(x['ns']) == 14]
                 items['subcats'].extend(subcats)
+                # 获取页面的标题和页面ID
                 pages = [(x['pageid'], x['title']) for x in results if int(x['ns']) == 0]
+                # 根据是否分组页面进行不同的处理
                 if group_pages:
                     if not items['pages'].get(category, None):
                         items['pages'][category] = []
                     items['pages'][category].extend(pages)
                 else:
                     items['pages'].extend(pages)
+                # 如果选择了递归，将子类别添加到类别列表中
                 if recursive:
                     # append new categories to the category list
                     items['categories'].extend(subcats)
+                # 检查是否有更多结果，获取下一页的cmcontinue值
                 if 'batchcomplete' in data.keys():
                     params['cmcontinue'] = 1
                 else:
                     params['cmcontinue'] = data['continue']['cmcontinue']
+            # 暂停1秒以避免连续请求造成过多负载
             time.sleep(1)
-        # prune duplicates (pages likely to re-occur across multiple subcategories)
+        # 去重并排序页面和子类别
         if not group_pages:
             for k, v in items.items():
                 items[k] = sorted(list(set(v)))
@@ -520,23 +530,188 @@ def sanitize_filename(filename):
     # 返回清理后的文件名。
     return filename
 
-def process_section(categroy_result, sections, depth=1):
+def process_section(sections, depth=1):
     markdown_sections = ""
 
     for section in sections:
         title = section.get('title', '')
         content = section.get('content', '')
 
-        markdown_sections += f"\n{'#' * depth} {title}\n\n{categroy_result}\n\n{content}\n"
+        markdown_sections += f"{'#' * depth} {title}\n\n{content}\n"
 
         subsections = section.get('sections', [])
         if subsections:
-            markdown_sections += process_section(categroy_result, subsections, depth + 1)
+            markdown_sections += process_section(subsections, depth + 1)
         
     return markdown_sections
 
+import copy
+from bs4 import BeautifulSoup, NavigableString, Tag
+from fandom import FandomPage
+import re
+
+class FandomPageParser(FandomPage):
+  @property
+  def content(self):
+    """
+    Text content of each section of the page, excluding images, tables,
+    and other data. The content is returned as dict, imitating the section and
+    subsection structure of the page.
+
+    .. note::
+      If you just want the plain text of the page without the section structure, you can use FandomPage.plain_text
+
+    :returns: :class:`dict`
+    """
+    def clean(content):
+      # 清洗内容的辅助函数，去除不需要的字符和标记
+      keys = list(content.keys())
+      if 'sections' in content: keys.remove('sections')
+
+      for key in keys:
+        if content[key] != "":
+          # 替换特殊字符
+          content[key] = re.sub(u'\xa0', ' ', content[key])
+          # 移除方括号标记
+          content[key] = re.sub(r'\[.*?\]', '', content[key])
+          # 简化多余空格和换行符
+          content[key] = re.sub(' +', ' ', content[key])
+          content[key] = re.sub('\n+', '\n', content[key])
+          # 如果内容只包含换行符，则设为空字符串
+          if content[key] == "\n":
+            content[key] = ""
+          else:
+            # 移除首尾的换行符
+            content[key] = content[key][1:] if content[key][0] == '\n' else content[key]
+            content[key] = content[key][:-1] if content[key][-1] == '\n' else content[key]
+
+      if 'sections' in content:
+        # 递归清洗每个子节
+        for s in content['sections']:
+          s = clean(s)
+
+      return content
+
+    if not getattr(self, '_content', False):
+      html = self.html
+      soup = BeautifulSoup(html, 'html.parser')
+
+       # 移除页面中的一些不需要的元素，如infobox，toc，message_boxes等
+      page_content = copy.copy(soup.find('div', class_="mw-parser-output"))
+
+      infoboxes = page_content.find_all('aside', class_="portable-infobox")
+      infobox_content = ""
+      for box in infoboxes:
+          infobox_content += box.text
+          box.decompose()
+
+      toc = page_content.find('div', id='toc')
+      if toc: toc.decompose()
+
+      message_boxes = page_content.find_all('table', class_="messagebox")
+      for box in message_boxes:
+        box.decompose()
+
+      captions = page_content.find_all('p', class_="caption")
+      for caption in captions:
+        caption.decompose()
+
+      nav_boxes = page_content.find_all('table', class_="navbox")
+      for box in nav_boxes:
+        box.decompose()
+
+      content = {'title': self.title}
+      level_tree = [content]
+      current_level = 1
+
+      # 初始化next_node为页面内容的第一个节点
+      next_node = page_content.contents[0]
+      while isinstance(next_node, NavigableString) or next_node.name in ["div", "figure"]:
+        # 跳过一些节点，如文本节点、div节点等
+        next_node = next_node.nextSibling
+
+      # 初始化节的文本内容为空字符串
+      section_text = ""
+      while True:
+        if next_node is None:
+          # 如果到达页面内容的末尾，则将当前节的文本内容设置为section_text，并结束循环
+          level_tree[-1]['content'] = section_text
+          break
+        elif isinstance(next_node, Tag):
+          if re.match(r'^h\d+', next_node.name):
+          #if next_node.name[0] == 'h':
+            # 如果当前节点是一个标题节点
+            level_tree[-1]['content'] = section_text
+            header = next_node.text
+            header_level = int(next_node.name[1])
+            if header_level > current_level:
+              # 如果当前标题级别高于当前级别，则添加一个新的子节
+              level_dif = abs(header_level - current_level)
+              for _ in range(level_dif):
+                level_tree[-1]['sections'] = [{'title':header}]
+                level_tree.append(level_tree[-1]['sections'][0])
+            elif header_level == current_level:
+              # 如果当前标题级别与当前级别相同，则在同一层级下添加一个新的子节
+              level_tree[-2]['sections'].append({'title':header})
+              level_tree[-1] = level_tree[-2]['sections'][-1]
+            else:
+              # 如果当前标题级别低于当前级别，则返回到正确的层级，并添加一个新的子节
+              level_dif = header_level - current_level
+              level_tree = level_tree[:level_dif]
+              if len(level_tree) >= 2:
+                level_tree[-2]['sections'].append({'title':header})
+                level_tree[-1] = level_tree[-2]['sections'][-1]
+
+            section_text = ""
+            # 清空section_text
+            current_level = header_level
+          #elif next_node.name == 'div':
+          # 如果当前节点不是printfooter类的节点，则将其文本内容添加到section_text中
+          elif (not next_node.has_attr('class')) or (next_node['class'][0] != "printfooter"):
+            # 如果是数据库表格，处理成文本
+            if next_node.name == 'table' and next_node.has_attr('class') and next_node['class'][0] == "article-table":
+                data_list = []
+
+                #找到表头
+                headers = next_node.select('th')
+                header_names = [header.text.strip() for header in headers]
+
+                # 找到数据行
+                rows = next_node.select('tr')
+                for row in rows:
+                    cells = row.select('td')
+                    if len(cells) == len(headers):
+                        data = {}
+                        for i, cell in enumerate(cells):
+                            data[header_names[i]] = cell.text.strip()
+                        data_list.append(data)
+
+                final_text = ""
+                # 输出每个表格中的数据
+                for data in data_list:
+                    for key, value in data.items():
+                        result = f"{key}: {value}"
+                        final_text += result
+                    final_text += "\n"
+                
+                section_text += final_text 
+            else:
+                section_text += "\n"+next_node.get_text()
+        next_node = next_node.nextSibling
+
+      # 如果当前节点不是printfooter类的节点，则将其文本内容添加到section_text中
+      if infobox_content != "": content['infobox'] = infobox_content
+
+      self._content = clean(content)
+    return self._content
+
 
 if __name__ == "__main__":
+
+    page = FandomPageParser(FANDOM_SITE, 'en', title='Ammunition', redirect=True, preload=False)
+    print(page.html)
+    sections = page.content['sections']
+
     print(f'Getting {CATEGORIES_RULES} infoboxes from fandom site {FANDOM_SITE}\n')
     # create WikiInfobox instance with default values
 
@@ -555,7 +730,8 @@ if __name__ == "__main__":
 
     for pageid,pagename in pages:
         time.sleep(1)
-        page = fandom.page(pagename)
+        #page = fandom.page(pagename)
+        page = FandomPageParser(FANDOM_SITE, 'en', title=pagename, redirect=True, preload=False)
         
         #额外获取分类信息
         query_params = {
@@ -575,7 +751,7 @@ if __name__ == "__main__":
         categroy_text = []
         for categroy in categories:
             title = categroy['title'].replace('Category:', '')
-            categroy_text.append(f"Category: {title}")
+            categroy_text.append(f"{title}")
 
         categroy_result = ",".join(categroy_text)        
 
@@ -584,16 +760,16 @@ if __name__ == "__main__":
         newfilename = sanitize_filename(pagename)
 
         print("try logging in file:" + newfilename)
-        with open(f"data/{newfilename}.md", "w", encoding='utf-8') as f:
+        with open(f"data/{newfilename}({categroy_result}).md", "w", encoding='utf-8') as f:
             try:
                 # 合并 `categories` 中的数据到 `page.content`
 
                 markdown = ''
                 if ('sections' in page.content):
                     sections = page.content['sections']
-                    markdown = process_section(categroy_result=categroy_result, sections=sections)
+                    markdown = process_section(sections=sections)
                 else:
-                    markdown = f"\n{'#'} {page.title}\n\n{categroy_result}\n\n{page.content}\n"
+                    markdown = f"\n{'#'} {page.title}\n\n{page.content}\n"
                 
 
                 #page.sections = categories + page.sections
